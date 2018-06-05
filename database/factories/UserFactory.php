2d462ee388c1bd8834e06a2e809181753c2729e2
Jean-Pierre Gassin <jeanpierre.gassin@gmail.com>
2018-06-05 16:26:31 +1000

    Never allow user nicknames to go beyond 20 characters

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,7 +18,7 @@ $factory->define(App\Models\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'nickname' => $setNickname ? $faker->userName : null,
+        'nickname' => $setNickname ? substr($faker->userName, 0, 20) : null,
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
         'remember_token' => str_random(10),
         'bio' => $faker->text,
